c7deccc90e81db9359b718691197e8984d85f558
Wasike-Elvis <wasikeelvis@gmail.com>
2019-12-01 09:27:29 +0300

    remove my form to change to a new form

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-var date = document.forms["Akan"]["Date"];
-var month = document.forms["Akan"]["Month"];
-function validate(){
-    if(date.value < 1 && date.value> 31){
-        date.style.border="2px color red";
-        return false;
-    }
-}
+$(document).ready(function(){
+    $("#myBirthDate").mask("99/99/9999");
+});
