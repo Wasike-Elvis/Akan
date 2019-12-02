1dff784d4a2475eac1f6c328bd0334d54ec9a00f
Wasike-Elvis <wasikeelvis@gmail.com>
2019-12-02 09:54:14 +0300

    add a change to my scrip

--- scripts.js
+++ scripts.js
@@ -1,25 +1,28 @@
-//getting date input from the form
-var mydate = parseInt(document.getElementById("mydate"));
-//var mydate = document.forms["akanform"]["mydate"];
-var gender = document.akanform.gender;
-
-var date = new Date("06-10-1997");
-var dateday = date.getDay();
-var weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-var day = weekdays[dateday];
-var mName =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-var fName = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-var valid = false; 
-
-//getting error display objects
-var mydateerror = document.getElementById('mydateerror');
-var gendererror = document.getElementById('gendererror');
-
-//get diplay object
-var display = document.getElementById('display');
 
 //validation function
+
 function validate(){
+
+    var mydate = parseInt(document.getElementById("mydate"));
+    //var mydate = document.forms["akanform"]["mydate"];
+    var gender = document.akanform.gender;
+    
+    var date = new Date("06-10-1997");
+    var dateday = date.getDay();
+    var weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    var day = weekdays[dateday];
+    var mName =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    var fName = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+    var valid = false; 
+    
+    //getting error display objects
+    var mydateerror = document.getElementById('mydateerror');
+    var gendererror = document.getElementById('gendererror');
+    
+    //get diplay object
+    var display = document.getElementById('display');
+    
+
     if(mydate.value == ""){
         mydate.style.backgroundColor = "#ff9900be";
         mydate.style.border = "2px groove red";
