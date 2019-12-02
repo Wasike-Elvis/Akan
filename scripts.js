10879dba80f1499c1371410f0b1bfe5166b2cf37
Wasike-Elvis <wasikeelvis@gmail.com>
2019-12-02 10:16:55 +0300

    add a function of day and name population function in the script page

--- scripts.js
+++ scripts.js
@@ -1,26 +1,28 @@
 
+
 //validation function
 
 function validate(){
 
-    var mydate = parseInt(document.getElementById("mydate"));
-    //var mydate = document.forms["akanform"]["mydate"];
-    var gender = document.akanform.gender;
-    
-    var date = new Date("06-10-1997");
-    var dateday = date.getDay();
-    var weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    var day = weekdays[dateday];
-    var mName =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-    var fName = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-    var valid = false; 
-    
-    //getting error display objects
-    var mydateerror = document.getElementById('mydateerror');
-    var gendererror = document.getElementById('gendererror');
-    
-    //get diplay object
-    var display = document.getElementById('display');
+    //getting date input from the form
+var mydate = document.getElementById("mydate");
+//var mydate = document.forms["akanform"]["mydate"];
+var gender = document.akanform.gender;
+
+var date = new Date(mydate.value);
+var dateday = date.getDay();
+var weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var day = weekdays[dateday];
+var mName =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+var fName = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+var valid = false; 
+
+//getting error display objects
+var mydateerror = document.getElementById('mydateerror');
+var gendererror = document.getElementById('gendererror');
+
+//get diplay object
+var display = document.getElementById('display'); 
     
 
     if(mydate.value == ""){
